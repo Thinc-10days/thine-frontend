24f417a125d06d643600d4950ad3da5c77a2789e
itsarasib <itsara.pliansart@gmail.com>
2023-10-22 03:06:28 +0700

    create login page

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,14 +1,52 @@
 // import { login } from "../../services/AuthService"
 
+import { Box, Button, Center, Input, Text } from "@chakra-ui/react";
+
 export const LoginPage = () => {
+  return (
+    <Center bg="white" h="100vh" color="#303030">
+      <Box
+        bgColor={"rgba(239, 174, 199, 0.50)"}
+        borderRadius={"16px"}
+        px="100px"
+        py="100px"
+        gap="20px"
+      >
+        <Text fontSize="3xl" fontWeight={700} mb="10px">
+          {" "}
+          username{" "}
+        </Text>
+        <Input
+          p={2}
+          placeholder="username"
+          bgColor={"white"}
+          w="400px"
+          mb="15px"
+        />
+        <Text fontSize="3xl" fontWeight={700} mb="10px">
+          {" "}
+          password{" "}
+        </Text>
+        <Input
+          p={2}
+          placeholder="password"
+          bgColor={"white"}
+          w="400px"
+          mb="15px"
+        />
+        <Box textAlign="right" mt="20px">
+          <Button p="10px 20px"> log in</Button>
+        </Box>
+      </Box>
+    </Center>
+  );
+};
 
-    // const username = 'tan'
-    // const password = '1234'
-
-    // const handleLogin = async () => {
-    //     await login(username, password)
-    // }
-    return (
-        <h1> log in page</h1>
-    )
+{
+  /* <Box my="150px" mx="100px">
+          <Text color="black">Username</Text>
+          <Input placeholder="username" bgColor={"white"} />
+          <Text color="black">Password</Text>
+          <Input placeholder="password" bgColor={"white"} />
+        </Box> */
 }
