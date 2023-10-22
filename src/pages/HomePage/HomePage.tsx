dd1fe80cb82b67f10c1da32d53013c1b6f1bf605
Sophitthakon <148642398+Sophitthakon@users.noreply.github.com>
2023-10-23 01:22:44 +0700

    Aom/home (#2)
    * Homepage update
    
    * Homepage update
    
    * color update

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,63 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Button, Center, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 export const HomePage: React.FC = () => {
+  const navigate = useNavigate();
   return (
-    <>
-      <Box bgColor="darkcyan"></Box>
-    </>
+    <Center h="100vh" bg="white">
+      <Box textAlign="center" display={"row"} px="100px" py="100px">
+        <Text fontSize="3xl" fontWeight={700}>
+          How are you feeling today!
+        </Text>
+        <Box
+          display="flex"
+          flexDir="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Button
+            bgColor={"white"}
+            margin={"20px"}
+            cursor="pointer"
+            _hover={{ bgColor: "#de5c8e" }}
+            onClick={() => navigate("/picker")}
+          >
+            Picker
+          </Button>
+          <Button
+            bgColor={"white"}
+            margin={"20px"}
+            cursor="pointer"
+            _hover={{ bgColor: "#de5c8e" }}
+            onClick={() => navigate("/eater")}
+          >
+            Eater
+          </Button>
+        </Box>
+      </Box>
+    </Center>
   );
 };
+
+/* <Card
+      alignItems="center"
+      bgColor={"green"}
+      borderRadius="10"
+      justifyContent={"center"}
+    >
+      <CardBody>
+        <Stack spacing="3">
+          <Heading fontSize="4xl">Role</Heading>
+        </Stack>
+      </CardBody>
+      <CardFooter>
+        <ButtonGroup spacing="20">
+          <Button variant="solid" bgColor="white">
+            DELIVER
+          </Button>
+          <Button variant="Solid" bgColor="white">
+            CUSTOMER
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card> */
