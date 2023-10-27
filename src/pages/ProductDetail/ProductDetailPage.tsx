18bd6d53ea172eec25b417ad687314d6fff343e7
Sophitthakon <sptkmm06@gmail.com>
2023-10-27 22:01:17 +0700

    update ProductDetailPage

--- src/pages/ProductDetail/ProductDetailPage.tsx
+++ src/pages/ProductDetail/ProductDetailPage.tsx
@@ -1,12 +1,21 @@
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../store/store";
-import { Grid, SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  SimpleGrid,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { MenuCard } from "../../Components/MenuCard";
 import { useEffect } from "react";
 import { setSelectedProduct } from "../../store/ProductSlice";
 
 const ProductDetailPage: React.FC = () => {
+  const { colorMode } = useColorMode();
   const { id } = useParams();
   const dispatch = useDispatch();
   const product = useSelector(
@@ -28,17 +37,39 @@ const ProductDetailPage: React.FC = () => {
   }
 
   return (
-    <Grid templateRows="2, 1fr">
-      <SimpleGrid columns={2} gap={"30px"}>
-        {product.food.map((food) => (
-          <MenuCard
-            name={food.name}
-            description={food.description}
-            price={food.price}
-            id="food"
-          />
-        ))}
-      </SimpleGrid>
+    <Grid templateRows="repeat(2, 1fr)" gap={"30px"}>
+      <GridItem
+        rowSpan={1}
+        backgroundImage={
+          "https://www.teenaagnel.com/wp-content/uploads/2019/12/food-photography-in-dubai.jpg"
+        }
+        borderRadius={"16px"}
+        border={`1px solid ${colorMode == "dark" ? "white" : "black"}`}
+        height={"full"}
+        overflow="hidden"
+      >
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+        >
+          <Heading>{product.name}</Heading>
+          <Text>{product.location}</Text>
+        </Box>
+      </GridItem>
+      <GridItem rowSpan={1}>
+        <SimpleGrid columns={2} gap={"30px"}>
+          {product.food.map((food) => (
+            <MenuCard
+              name={food.name}
+              description={food.description}
+              price={food.price}
+              id="food"
+            />
+          ))}
+        </SimpleGrid>
+      </GridItem>
     </Grid>
   );
 };
